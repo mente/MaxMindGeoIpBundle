363cf2f50e4295dc0846d98fffb7cdef62d3ad7b
dontub <dtubach@web.de>
2019-08-08 15:25:15 +0200

    Avoid deprecated TreeBuilder usage

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('insomnia_geo_ip');
+        $treeBuilder = new TreeBuilder('insomnia_geo_ip');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('insomnia_geo_ip');
+        }
 
         $rootNode
             ->children()
