bd1b1cd76ce7955c010f923ff7fe23e2da5da9b7
mente <aa.vasilenko@gmail.com>
2019-08-08 16:56:19 +0200

    verify generated configuration

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -11,10 +11,12 @@ class ConfigurationTest extends TestCase
     public function testConfigurationCompiles()
     {
         $processor = new Processor();
-        $processor->processConfiguration(new Configuration(), [
+        $config = $processor->processConfiguration(new Configuration(), [
             'insomnia_max_mind_geo_ip' => [
-                'path' => '%kernel.root_dir%/geolite2.mmdb'
+                'path' => 'some-path'
             ]
         ]);
+
+        $this->assertEquals($config['path'], 'some-path');
     }
 }
